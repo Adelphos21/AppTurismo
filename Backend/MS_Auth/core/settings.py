18aa2867679b2af6548c4fd75700e3da38d8ba1a
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2025-10-07 03:12:25 +0000

    cambios menores finales

--- Backend/MS_Auth/core/settings.py
+++ Backend/MS_Auth/core/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-7d4vm9s0kcypl1rki8b*dp5c(!+6ayz6s73pi98ekn4n)e=ps9
 DEBUG = True
 
 ALLOWED_HOSTS = [
-'34.224.8.229',
-'54.167.202.129'
+'*'
 ]
 
 # Application definition
